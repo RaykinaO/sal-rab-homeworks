5c29ea6751a68bbadea639afaa3d44fb62964e43
Olga Raykina <yallo69@mail.ru>
2022-12-16 19:59:28 +0300

    Tast_final_fixed

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,16 +33,16 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, goods: [title, count], order: {address, sum}};
+function sendRequest(name, phone, address, goods, sum){
+    let data = {client: {name, phone}, goods: [], order: {address, sum}};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push({ title: goods[i].title, count: goods[i].count });
+        data.goods.push({title: goods[i].title, count: goods[i].count });
     }
 
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж , кв ${address.flat}`.;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
 
     data.client = `${name} ${phone}`;
